28b44edc713659624ce2a6db77b3f28c6a1dc626
Matthew Johnson <dev.shell.email@gmail.com>
2020-02-13 21:19:11 -0600

    Buildable jar with maven

---
+++ src/main/java/com/matthewjohnson42/personalMemexService/PersonalMemexServiceApplication.java
@@ -0,0 +1,13 @@
+package com.matthewjohnson42.personalMemexService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalMemexServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalMemexServiceApplication.class);
+	}
+
+}
