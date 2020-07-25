99305ffc85889812527564ef4d8b715f07ffda76
Matthew Johnson <dev@matthewjohnson42.com>
2020-07-25 04:08:27 -0500

    Update request logger to log URIs of invalid requests

--- src/main/java/com/matthewjohnson42/personalMemexService/web/interceptor/RequestLoggingInterceptor.java
+++ src/main/java/com/matthewjohnson42/personalMemexService/web/interceptor/RequestLoggingInterceptor.java
@@ -5,6 +5,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.web.servlet.HandlerInterceptor;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletRequestWrapper;
 import javax.servlet.http.HttpServletResponse;
 
 /**
@@ -16,7 +17,15 @@ public class RequestLoggingInterceptor implements HandlerInterceptor {
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
-        logger.info(String.format("method=\"%s\" url=\"%s\" query=\"%s\"", request.getMethod(), request.getRequestURL(), request.getQueryString()));
+        String requestUri = request.getRequestURI();
+        HttpServletRequestWrapper reqWrapper = null;
+        if (request instanceof HttpServletRequestWrapper) {
+            reqWrapper = (HttpServletRequestWrapper) request;
+        }
+        if (reqWrapper != null && reqWrapper.getRequest() != null && reqWrapper.getRequest().getAttribute("javax.servlet.error.request_uri") != null) {
+            requestUri = reqWrapper.getRequest().getAttribute("javax.servlet.error.request_uri").toString();
+        }
+        logger.info(String.format("method=\"%s\" url=\"%s\" query=\"%s\"", request.getMethod(), requestUri, request.getQueryString()));
         return true;
     }
 
