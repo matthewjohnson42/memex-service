7d5a39be4b998c75b30ece9fce18a3f84639c9f7
Matthew Johnson <dev@matthewjohnson42.com>
2021-02-20 23:08:03 -0600

    Update build scripts to match changes in personal-memex-server

--- docker/docker-compose-up.sh
+++ docker/docker-compose-up.sh
@@ -3,7 +3,9 @@
 # script for building docker images and starting them using docker-compose
 # assumes directory of invocation is personal-memex-service/docker, parent of this script
 
-sh docker-build.sh
+docker build --tag memex-mongo:0.0.1 --file mongo/Dockerfile ..
+mvn clean install -f ../app/pom.xml
+docker build --tag memex-service:0.0.1 --file app/Dockerfile ..
 docker-compose --file mongo/docker-compose.yml up --detach
 docker-compose --file elasticsearch/docker-compose.yml up --detach
 echo "waiting for elastic to initialize" && sleep 15
