4e383098682caddb89a7a2f8b7023eb980bd1941
Matthew Johnson <mjohnson@omega-fortytwo.local>
2021-02-21 22:39:33 -0600

    Update Mongo DB name

--- docker/mongo/dbInit.sh
+++ docker/mongo/dbInit.sh
@@ -2,4 +2,4 @@
 
 sleep 10
 cat /dbInit.js | sed "s/\${MONGO_DEFAULT_USER_PW}/${MONGO_DEFAULT_USER_PW}/g" > /dbInitInterpolated.sh
-mongo --host ${MONGO_HOST}:27017 < /dbInitInterpolated.sh
+mongo --host ${MONGO_HOST}:27017/memex < /dbInitInterpolated.sh
