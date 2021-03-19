4e5e5565f95f45629b2b313e88c08fdbfcfe79fb
Matthew Johnson <dev@matthewjohnson42.com>
2021-03-18 21:32:40 -0500

    Add CORS configuration

--- app/src/main/java/com/matthewjohnson42/personalMemexService/config/WebSecurityConfiguration.java
+++ app/src/main/java/com/matthewjohnson42/personalMemexService/config/WebSecurityConfiguration.java
@@ -1,36 +1,68 @@
 package com.matthewjohnson42.personalMemexService.config;
 
 import com.matthewjohnson42.personalMemexService.web.filter.auth.CustomJwtGrantedAuthoritiesConverter;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Profile("prod")
 @EnableWebSecurity
 @Configuration
 public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
 
-    @Autowired
-    CustomJwtGrantedAuthoritiesConverter customJwtGrantedAuthConverter;
+    private List<String> allowedOrigins = new ArrayList<>();
+
+    private final CustomJwtGrantedAuthoritiesConverter customJwtGrantedAuthConverter;
+
+    public WebSecurityConfiguration(
+            CustomJwtGrantedAuthoritiesConverter customJwtGrantedAuthConverter,
+            @Value("${cors.allowed-origins}") String allowedOriginsFromYml) {
+        this.customJwtGrantedAuthConverter = customJwtGrantedAuthConverter;
+        List<String> allowedOriginList = List.of(allowedOriginsFromYml.split(","));
+        for (String origin: allowedOriginList) {
+            allowedOrigins.add(origin.trim());
+        }
+    }
 
     @Override
     protected void configure(HttpSecurity httpSecurity) throws Exception {
-        JwtAuthenticationConverter jwtAuthenticationConverter = new JwtAuthenticationConverter();
-        jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(customJwtGrantedAuthConverter);
         httpSecurity
                 .authorizeRequests()
                 .antMatchers("/api/v0/auth").permitAll()
                 .antMatchers("/**").authenticated()
                 .and().csrf().ignoringAntMatchers("/api/v0/auth")
-//                .and().cors() // todo need to configure CORS
-//                .and().requiresChannel().anyRequest().requiresSecure() // todo this can be done using Nginx
+                .and().cors().configurationSource(
+                    urlBasedCorsConfigurationSource()
+                )
                 .and().oauth2ResourceServer().jwt(
-                    jwtConfigurer -> jwtConfigurer.jwtAuthenticationConverter(jwtAuthenticationConverter)
+                    jwtConfigurer -> jwtConfigurer.jwtAuthenticationConverter(jwtAuthenticationConverter())
                 );
+    }
+
+    JwtAuthenticationConverter jwtAuthenticationConverter() {
+        JwtAuthenticationConverter jwtAuthenticationConverter = new JwtAuthenticationConverter();
+        jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(customJwtGrantedAuthConverter);
+        return jwtAuthenticationConverter;
+    }
+
+    UrlBasedCorsConfigurationSource urlBasedCorsConfigurationSource() {
+        UrlBasedCorsConfigurationSource urlBasedCorsConfigurationSource = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.setAllowedOrigins(allowedOrigins);
+        corsConfiguration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        corsConfiguration.addAllowedHeader("*");
+        corsConfiguration.setMaxAge(1800L);
+        urlBasedCorsConfigurationSource.registerCorsConfiguration("/**/*", corsConfiguration);
+        return urlBasedCorsConfigurationSource;
     }
 
 }
