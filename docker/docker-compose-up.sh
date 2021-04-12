a82e088e30298ae334cb8bbae84a33a34d1f40e8
Matthew Johnson <mjohnson@omega-fortytwo.local>
2021-04-11 20:49:50 -0500

    Update dev configuration, add default dev config, update docker-compose script

--- docker/docker-compose-up.sh
+++ docker/docker-compose-up.sh
@@ -3,9 +3,35 @@
 # script for building docker images and starting them using docker-compose
 # assumes directory of invocation is personal-memex-service/docker, parent of this script
 
-docker build --tag memex-mongo:0.0.1 --file mongo/Dockerfile ..
+if [[ -z ${MONGO_DEFAULT_USER_PW} ]]; then
+  echo "Enter value for MONGO_DEFAULT_USER_PW: "
+  read MONGO_DEFAULT_USER_PW
+  export MONGO_DEFAULT_USER_PW=${MONGO_DEFAULT_USER_PW}
+fi
+
+if [[ -z ${TOKEN_ENC_KEY_SECRET} ]]; then
+  echo "Enter value for TOKEN_ENC_KEY_SECRET: "
+  read TOKEN_ENC_KEY_SECRET
+  export TOKEN_ENC_KEY_SECRET=${TOKEN_ENC_KEY_SECRET}
+fi
+
+if [[ -z ${USERPASS_ENC_KEY_SECRET} ]]; then
+  echo "Enter value for USERPASS_ENC_KEY_SECRET: "
+  read USERPASS_ENC_KEY_SECRET
+  export USERPASS_ENC_KEY_SECRET=${USERPASS_ENC_KEY_SECRET}
+fi
+
+if [[ -z ${SPRING_PROFILES_ACTIVE} ]]; then
+  echo "Enter value for SPRING_PROFILES_ACTIVE (empty for dev): "
+  read SPRING_PROFILES_ACTIVE
+  export SPRING_PROFILES_ACTIVE=${SPRING_PROFILES_ACTIVE}
+fi
+
+# build
 mvn clean install -f ../app/pom.xml
 docker build --tag memex-service:0.0.1 --file app/Dockerfile ..
+
+# start containers
 docker-compose --file mongo/docker-compose.yml up --detach
 echo "waiting for mongo to initialize" && sleep 5
 cat mongo/dbInit.js | sed "s/\${MONGO_DEFAULT_USER_PW}/${MONGO_DEFAULT_USER_PW}/g" > mongo/dbInitInterpolated.js
