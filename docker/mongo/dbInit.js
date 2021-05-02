e2a40f2fa49b49862c464bc8c8070d32da863c1e
Matthew Johnson <mjohnson@omega-fortytwo.local>
2021-05-02 16:13:07 -0500

    Update Mongo init query to add default user only when no users exist

--- docker/mongo/dbInit.js
+++ docker/mongo/dbInit.js
@@ -2,11 +2,15 @@
 use memex;
 now = new ISODate();
 
-db.userDetails.insertOne({
-    "_id" : "default",
-    "password" : "${MONGO_DEFAULT_USER_PW}",
-    "authorities" : [],
-    "createDateTime" : now,
-    "updateDateTime" : now,
-    "_class" : "com.matthewjohnson42.memexService.data.mongo.entity.UserDetailsMongo"
-});
+const users = db.userDetails.find({}).toArray();
+
+if (users.length === 0) {
+    db.userDetails.insertOne({
+        "_id" : "default",
+        "password" : "${MONGO_DEFAULT_USER_PW}",
+        "authorities" : [],
+        "createDateTime" : now,
+        "updateDateTime" : now,
+        "_class" : "com.matthewjohnson42.memexService.data.mongo.entity.UserDetailsMongo"
+    });
+}
